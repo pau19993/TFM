8686e3aee5acdcadacc2586462750bb92ef6c6bb
pau19993 <pau_castano@hotmail.com>
2023-05-23 11:52:57 +0200

    Simple

---
+++ simple.py
@@ -0,0 +1,34 @@
+import numpy as np
+import classify
+import tflite_runtime.interpreter as tflite
+import time
+
+from PIL import Image
+
+
+interpreter = tflite.Interpreter(model_path="model.tflite")
+interpreter.allocate_tensors()
+
+
+input_index = interpreter.get_input_details()[0]['index']
+output_index = interpreter.get_output_details()[0]['index']
+
+size = classify.input_size(interpreter)
+image = Image.open('images.jpg').convert('RGB').resize(size, Image.ANTIALIAS)
+classify.set_input(interpreter, image)
+
+"""
+interpreter.set_tensor(input_index, features.astype(np.float32))
+interpreter.invoke()
+prediction = interpreter.get_tensor(output_index)
+"""
+
+start = time.perf_counter()
+interpreter.invoke()
+inference_time = time.perf_counter() - start
+classes = classify.get_output(interpreter, 1, 0)
+print('%.1fms' % (inference_time * 1000))
+
+print('-------RESULTS--------')
+for klass in classes:
+    print('%s: %.5f' % (labels.get(klass.id, klass.id), klass.score))
