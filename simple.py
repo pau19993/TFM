0b2e12dc6d3a56670c5f9de22f4870cf64bd1080
pau19993 <pau_castano@hotmail.com>
2023-06-06 19:46:35 +0200

    ojala

--- simple.py
+++ simple.py
@@ -11,30 +11,28 @@ input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
 
 # Tamaño de entrada esperado por el modelo
-input_shape = (31, 31)
+input_shape = (31, 31, 1)  # Actualizar a una dimensión de tamaño 4
 
 # Clases de ropa correspondientes a las salidas del modelo
 class_names = ['Camiseta', 'Pantalón', 'Suéter', 'Vestido', 'Abrigo', 'Sandalia', 'Camisa', 'Zapatilla deportiva', 'Bolso', 'Bota']
 
 # Seleccionar una imagen de la carpeta
 image_path = 'images.jpg'
-image = Image.open(image_path).resize(input_shape)
+image = Image.open(image_path).resize(input_shape[:2])  # Redimensionar solo las dimensiones espaciales
 
 # Preprocesamiento de la imagen
 image_array = np.array(image) / 255.0
 
 # Asegurarse de que las dimensiones de la imagen sean correctas
-if image_array.shape != input_shape:
-    image_array = np.resize(image_array, input_shape)
+if image_array.shape != input_shape[:2]:
+    image_array = np.resize(image_array, input_shape[:2])
 
-# Agregar una dimensión adicional para representar el batch de imágenes
+# Agregar una dimensión adicional para representar el batch de imágenes y el canal
 input_data = np.expand_dims(image_array, axis=0)
+input_data = np.expand_dims(input_data, axis=-1)
 
 # Convertir el tipo de datos a FLOAT32
 input_data = input_data.astype(np.float32)
-
-# Ajustar las dimensiones del tensor de entrada si es necesario
-input_data = np.reshape(input_data, (1,) + input_shape + (1,))
 
 # Establecer los datos de entrada del modelo
 interpreter.set_tensor(input_details[0]['index'], input_data)
